c5cbb7cf3d1273f7ee85802432f45a530e66c788
DestroyerMob <ethanhellyer7@gmail.com>
2026-07-12 15:50:08 +1000

    fixed up carryon

--- pack/kubejs/server_scripts/remove_direct_equipment_recipes.js
+++ pack/kubejs/server_scripts/remove_direct_equipment_recipes.js
@@ -3,6 +3,7 @@ ServerEvents.recipes(event => {
   const armorSlots = ['helmet', 'chestplate', 'leggings', 'boots']
   const moreWeaponsMaterials = ['wooden', 'stone', 'copper', 'iron', 'golden', 'diamond', 'netherite']
   const moreWeaponsTypes = ['battle_axe', 'great_sword', 'katana', 'knife', 'machete', 'spear']
+  const farmersDelightKnifeMaterials = ['flint', 'iron', 'golden', 'diamond', 'netherite']
 
   armorMaterials.forEach(material => {
     armorSlots.forEach(slot => {
@@ -14,5 +15,9 @@ ServerEvents.recipes(event => {
     moreWeaponsTypes.forEach(type => {
       event.remove({ output: `mobsmoreweapons:${material}_${type}` })
     })
+  })
+
+  farmersDelightKnifeMaterials.forEach(material => {
+    event.remove({ output: `farmersdelight:${material}_knife` })
   })
 })
